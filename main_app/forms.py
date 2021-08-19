19bbee28428bded7b6804297a9208fc9d012140e
selaguardia <selaguardia91@gmail.com>
2021-08-19 11:09:38 -0700

    merged with dev

--- main_app/forms.py
+++ main_app/forms.py
@@ -10,3 +10,9 @@ class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ('image', )
+class SignUpForm(UserCreationForm):
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+
+    class Meta:
+        model = User
+        fields = ('username', 'birth_date', 'password1', 'password2', )
