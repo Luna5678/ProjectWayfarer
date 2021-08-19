a604490f632ac67ee4b7a49d2b721a1e62878739
gtiamzon <gerald.tiamzon@yahoo.com>
2021-08-19 10:12:49 -0700

    commit for pull

--- main_app/views.py
+++ main_app/views.py
@@ -4,34 +4,32 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Profile
-from django.contrib.auth.forms import UserCreationForm
-
 
 # Create your views here.
 
-# class UserForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ('username','password','email','first_name', 'last_name')
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username','password','email','first_name', 'last_name')
 
 class ProfileForm(ModelForm):
     class Meta:
         model= Profile
         fields = ('profile_img', 'current_city')
-
 class Signup(View):
 
-    def get(self, request):
-        user_form = UserCreationForm()
+    def get(self,request):
         profile_form = ProfileForm()
-        # user_form = UserForm()
+        user_form = UserForm()
         context = {"profile_form": profile_form, "user_form": user_form}
         return render(request, "registration/signup.html", context)
 
 
-    def post(self, request):
-        user_form = UserCreationForm(request.POST)
+
+
+    def post(self,request):
         profile_form = ProfileForm(request.POST)
+        user_form = UserForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             # create our user in the database
             user = user_form.save()
