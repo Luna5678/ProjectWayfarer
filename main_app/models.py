2bfc451aad9012a655fc15350b1e9e51470e2cbf
nicknamejv <jonvercabral@yahoo.com>
2021-08-18 20:21:37 -0700

    tried to refractor UserForms to include more fields

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 from django.db.models import Model, CharField, OneToOneField
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 
 # Create your models here.
 class Profile(Model):
-  user = OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+  user = OneToOneField(User, on_delete=models.CASCADE,related_name="profile")
   current_city = CharField(max_length=50)
   profile_img = CharField(max_length=500)
 
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
   def __str__(self):
-    return self.user, current_city
+    return self.user.username
 
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -4,32 +4,34 @@ from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ('username','password','email','first_name', 'last_name')
+# class UserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ('username','password','email','first_name', 'last_name')
 
 class ProfileForm(ModelForm):
     class Meta:
         model= Profile
         fields = ('profile_img', 'current_city')
+
 class Signup(View):
 
-    def get(self,request):
+    def get(self, request):
+        user_form = UserCreationForm()
         profile_form = ProfileForm()
-        user_form = UserForm()
+        # user_form = UserForm()
         context = {"profile_form": profile_form, "user_form": user_form}
         return render(request, "registration/signup.html", context)
 
 
-
-
-    def post(self,request):
+    def post(self, request):
+        user_form = UserCreationForm(request.POST)
         profile_form = ProfileForm(request.POST)
-        user_form = UserForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             # create our user in the database
             user = user_form.save()
