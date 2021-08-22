48558d128b87055b32070681735712dfe0a843d7
selaguardia <selaguardia91@gmail.com>
2021-08-22 15:07:10 -0700

    cities view page dtl works on left side

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,3 +1,7 @@
 $('.modal').on('shown.bs.modal', function() {
     $(this).find('[autofocus]').focus();
 });
+
+// $('#left_city_selector').click( function() {
+//     $('#city_post_name').innerHTML('{{ city.city }}');
+// });
