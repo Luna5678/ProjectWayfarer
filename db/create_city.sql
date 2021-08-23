08a56ebad389aea137d545e473a1268c109689f5
nicknamejv <jonvercabral@yahoo.com>
2021-08-23 09:45:22 -0700

    modal works lets go

--- db/create_city.sql
+++ db/create_city.sql
@@ -8,7 +8,6 @@ INSERT INTO main_app_city(city, country, image)
     ('San Francisco', 'United States', 'https://www.artandarchitecture-sf.com/wp-content/uploads/2011/08/DSC_0043-4.jpg'),
     ('Seattle', 'United States', 'https://cdn.pixabay.com/photo/2019/01/18/21/23/chewing-gum-3940711_960_720.jpg'),
     ('Sydney', 'Australia', 'https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2017/09/redfern-mural--jam-project_flickr.jpg'),
-    ('Berlin', 'Germany', 'https://www.bu.edu/globalprograms/files/2011/01/Berlin-Germany-Wall-of-Berlin-Graffiti-Banner.jpg'),
     ('Singapore', 'Singapore', 'https://res.klook.com/image/upload/c_fill,w_750,h_500,f_auto/w_80,x_15,y_15,g_south_west,l_klook_water/activities/mcegxphankqeujwbhpjp.jpg'),
     ('Sydney', 'Austrailia', 'https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2017/09/redfern-mural--jam-project_flickr.jpg'),
     ('Tokyo', 'Japan', 'https://japansaucedotnet.files.wordpress.com/2018/12/Original-Gundam.jpg?w=700')
