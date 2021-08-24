3f3ea576164d78cf429e7a8830f9ccf96dbe80b6
selaguardia <selaguardia91@gmail.com>
2021-08-24 16:55:49 -0700

    modified heroku settings

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os 
 import dotenv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,5 +137,4 @@ LOGOUT_REDIRECT_URL = '/'
 # Other settings above
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
